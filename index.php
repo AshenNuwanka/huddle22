629b40ba576336dcce4b96c1fc77878dffcd7a5e
AshenNuwanka <ashennuwanka15@gmail.com>
2023-02-21 00:28:03 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Huddle</title>
+
+  <link rel="icon" href="./images/icon.png" />
+
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="bootstrap.css" />
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+
+<body class="bgcol">
+  <!-- container -->
+  <div class="container-fluid">
+
+    <!-- logo -->
+    <div class="row">
+      <div class="col-12">
+        <img src="images/logo.svg" class="logo m-5">
+      </div>
+    </div>
+    <!-- logo end-->
+
+    <!-- main body -->
+    <div class="row px-5">
+
+      <div class="col-12 col-lg-7">
+        <img src="images/mainIMG.svg" class="mimg w-100">
+      </div>
+
+      <div class="col-12 col-lg-5 text-md-start text-center mt-5">
+        <h2 class="text-white fw-bold">Build The Community<br />Your Fans Will Love</h2>
+
+        <p class="paragraph"> Huddle re-imagines the way we build communities. You have a voice, but so does your audience.
+          Create connections with your users as you engage in genuine discussion</p>
+
+        <button class="registerBTN">Register</button>
+
+      </div>
+
+    </div>
+    <!-- main body end-->
+
+    <!-- social-link -->
+    <div class="row px-5">
+      <div class="col-12 text-md-end text-center">
+
+        <li class="list-inline-item">
+          <a href="#" class="form-floating">
+          <i class="bi bi-facebook iconcol" style="font-size: 30px;"></i>
+          </a>
+        </li>
+
+        <li class="list-inline-item">
+          <a href="#" class="form-floating">
+          <i class="bi bi-twitter iconcol" style="font-size: 30px;"></i>
+          </a>
+        </li>
+
+        <li class="list-inline-item">
+          <a href="#" class="form-floating">
+          <i class="bi bi-instagram iconcol" style="font-size: 30px;"></i>
+          </a>
+        </li>
+
+      </div>
+    </div>
+    <!-- social-link end-->
+
+  </div>
+  </div>
+  <!-- container end-->
+</body>
+
+</html>
